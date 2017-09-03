7c6cbc6361f315e0c190648ccd3c1e03f77bbea1
Computador Robert <janderson.rdsilva@gmail.com>
2017-09-03 20:03:40 -0300

    Continuando

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View, TextInput} from 'react-native';
 import Button from 'react-native-button';
 import DatePicker from 'react-native-datepicker'
 
@@ -10,14 +10,22 @@ export default class App extends React.Component {
     this.state = {
       date_birth:"03/03/2017",
       date_prev_birth:"03/05/2017",
-      corrected_date: "Oi"
+      corrected_date: "Oi",
+      weeks_gestacional_age: "0",
+      days_gestacional_age: "0"
     }
   }
 
   calc() {
     let date_today = new Date();
     let date_birth = new Date(this.state.date_birth);
-    let date_prev_birth = new Date(this.state.date_prev_birth);
+    
+    let weeks_gestacional_age = parseInt(this.state.weeks_gestacional_age);
+    let days_gestacional_age = parseInt(this.state.days_gestacional_age);
+    days_gestacional_age += (weeks_gestacional_age * 7);
+    console.log(days_gestacional_age);
+    let date_prev_birth = new Date(date_birth.getTime() + 86400 * days_gestacional_age);
+    this.state.date_prev_birth = date_prev_birth.toLocaleString();
     let life_time = date_today.getTime() - date_birth.getTime();
     let life_time_corrected = date_today.getTime() - date_birth.getTime();
 
@@ -41,7 +49,7 @@ export default class App extends React.Component {
         <Text>Data de Nascimento</Text>
         <DatePicker
           style={{width: 200}}
-          date={this.state.date_prev_birth}
+          date={this.state.date_birth}
           mode="date"
           placeholder="select date"
           format="DD/MM/YYYY"
@@ -63,11 +71,25 @@ export default class App extends React.Component {
           }}
           onDateChange={(date) => {this.setState({date_birth: date})}}
         />
+        <Text>Idade Gestacional</Text>
+        <Text>Semanas</Text>
+        <TextInput
+          style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 1}}
+          onChangeText={(text) => this.setState({weeks_gestacional_age: text})}
+          value={this.state.weeks_gestacional_age}
+        />
+        <Text>Dias</Text>
+        <TextInput
+          style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 1}}
+          onChangeText={(text) => this.setState({days_gestacional_age: text})}
+          value={this.state.days_gestacional_age}
+        />
         <Text>Data de Prevista de Nascimento</Text>
+        <Text>{this.state.date_prev_birth}</Text>
 
-        <DatePicker
+        {/* <DatePicker
           style={{width: 200}}
-          date={this.state.date_birth}
+          date={this.state.date_prev_birth}
           mode="date"
           placeholder="select date"
           format="DD/MM/YYYY"
@@ -88,7 +110,7 @@ export default class App extends React.Component {
             // ... You can check the source to find the other keys. 
           }}
           onDateChange={(date) => {this.setState({date_prev_birth: date})}}
-        />
+        /> */}
 
         <Button
           style={{fontSize: 20, color: 'green'}}
@@ -96,6 +118,7 @@ export default class App extends React.Component {
           onPress={() => this.calc()}>
           Calcular! :)
         </Button>
+        <Text>Idade Gestacional Corrigida (semanas)</Text>
         <Text>{this.state.corrected_date}</Text>
       </View>
     );
